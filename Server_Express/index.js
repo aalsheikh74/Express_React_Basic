acdcecf61356fe6aca5a87194b92e74955e55f75
Ashraf Alsheikh <alsheikh813@gmail.com>
2018-12-16 20:29:06 +0200

    (feat) Create simple React App

--- Server_Express/index.js
+++ Server_Express/index.js
@@ -18,8 +18,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //     res.send("<h1>Welcome to Express</h1>");
 // });
 
-// Serve any static files
-app.use(express.static(path.join(__dirname, '../Client_Static_HTML_File')));
+// // Serve any static files
+// app.use(express.static(path.join(__dirname, '../Client_Static_HTML_File')));
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+  
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
